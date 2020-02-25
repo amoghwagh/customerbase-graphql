83203c34193996f80ece2a6f3755494af51dd3de
Amogh Wagh <amoghwagh@gmail.com>
2020-02-25 18:46:58 +0530

    Added customers and hardcoded customer data

--- schema.js
+++ schema.js
@@ -7,6 +7,28 @@ const {
   GraphQLNonNull
 } = require("graphql");
 
+// Hardcoded Data
+const customers = [
+  {
+    id: "1",
+    name: "John Doe",
+    email: "jdoe@gmail.com",
+    age: 35
+  },
+  {
+    id: "2",
+    name: "Jane Doe",
+    email: "janedoe@gmail.com",
+    age: 25
+  },
+  {
+    id: "3",
+    name: "Steve Smith",
+    email: "stevesmith@gmail.com",
+    age: 23
+  }
+];
+
 // Customer Type
 const CustomerType = new GraphQLObjectType({
   name: "Customer",
@@ -21,8 +43,28 @@ const CustomerType = new GraphQLObjectType({
 // Root Query
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
-  customer: {
-    type: CustomerType
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id == args.id) {
+            return customers[i];
+          }
+        }
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args) {
+        return customers;
+      }
+    }
   }
 });
-module.exports = new GraphQLSchema({});
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
